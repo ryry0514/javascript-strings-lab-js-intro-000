a805f8902f07147158322c68de38b7ea7f3a2c3b
astounding-modem-6998 <laylasdadddyy05@gmail.com>
2020-03-01 11:46:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var greeting= "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-''
+var greetSpecialGuest = "Hello, Neil deGrasse Tyson"
+
 var topic = "space";
 
 var conversation = `${topic}`;
